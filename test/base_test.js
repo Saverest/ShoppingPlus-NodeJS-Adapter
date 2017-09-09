5b71bd48a4183925ba0ee8601125b4f02bce6bdd
Filippo Berto <filippoberto95@gmail.com>
2017-09-09 09:38:48 +0200

    initial commit

---
+++ test/base_test.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let chai = require("chai");
+
+chai.config.includeStack = true;
+
+const expect = chai.expect;
+
+const DBAccess = require("../DBAccess");
+
+describe("DBAccess", () => {
+	context("#getUserData", function () {
+		it("test", function (done) {
+
+
+				done();
+
+
+		});
+
+	});
+});
